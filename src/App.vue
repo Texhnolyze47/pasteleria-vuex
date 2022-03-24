2e289e2e4f27eedcecc4d3afedc2a6918343db4f
Texhnolyze <ichigobankai136@gmail.com>
2022-03-24 01:24:18 -0600

    add: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="search">
+      <form action="#">
+        <input type="text" placeholder=" Search Courses" name="search" />
+        <button>
+          <box-icon name="search"></box-icon>
+        </button>
+      </form>
+    </div>
+    <div class="container-title">
+      <h1 class="title-pink">Pasteleria</h1>
+      <h1 class="title-blue">Fabricio</h1>
+    </div>
+    <div>
+      <box-icon class="icon" name="cart" color="#ff759f"></box-icon>
+      <box-icon  name="user-circle" color="#ff759f"></box-icon>
+    </div>
+    <div class="container">
+      <ul>
+        <li>Home</li>
+        <li>Pasteles</li>
+        <li>Personaliza tu pastel</li>
+        <li>Acerca de</li>
+        <li>Contáctenos</li>
+      </ul>
+    </div>
   </nav>
-  <router-view/>
 </template>
 
 <style>
@@ -16,7 +38,11 @@
 }
 
 nav {
-  padding: 30px;
+  padding-bottom: 10px;
+  background-color: #fff9fa;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
 }
 
 nav a {
@@ -26,5 +52,59 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+/* styling search bar */
+.search input[type="text"] {
+  position: absolute;
+  flex-basis: 40%;
+  width: 300px;
+  height: 25px;
+  border-radius: 25px;
+  border: solid 1px #ff759f;
+}
+form{
+  padding-right: 350px;
+}
+
+.search {
+  float: left;
+}
+
+.search button {
+  position: relative;
+  background-color: rgb(255, 255, 255, 0.1);
+  color: #f2f2f2;
+  margin-left: 270px;
+  margin-top: 1px;
+  margin-right: 16px;
+  font-size: 12px;
+  border: none;
+  cursor: pointer;
+}
+.container-title {
+  display: flex;
+  padding-right: 450px;
+  
+}
+.title-pink {
+  color: #ff759f;
+  padding-right: 10px;
+}
+.title-blue {
+  color: #5bc0eb;
+}
+
+.icon{
+  margin-right: 15px;
+}
+
+.container{
+  padding-left: 500px;
+}
+.container ul li{
+  display: inline-flex;
+  list-style: none;
+  padding-right: 20px;
 }
 </style>
