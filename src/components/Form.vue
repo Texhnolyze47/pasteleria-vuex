2dca184d8e48a31b3feb3216558f9e82353f2df5
Texhnolyze <ichigobankai136@gmail.com>
2022-03-25 17:14:50 -0600

    add: se actualizo la info del formulario

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,48 +1,50 @@
 <template>
-<div class="layout">
+  <div class="layout">
     <div class="mitad">
-    <form id="form-pe">
-      <p>Escoge uno </p>
-  <input type="text" />
-  <br/>
-  <label for="sabor">Sabor de pastel</label>
+      <form id="form-pe">
+        <h2>Informacion Personal</h2>
+        <br />
+        <label class="margin-right10" for="sabor">Numero de teléfono</label>
+        <input type="text" />
+        <br />
+        <br />
 
-        <p>Escoge uno </p>
-  <input type="text"  />
-  <br/>
-  <label for="sabor">Adorno del pastel</label>
-<div>
-      <button class="margin-top10">Enviar pedido</button>
-</div>
- 
-  </form>
-  
-</div>
-<div class="mitad">
-    <h2 class="title">Sabores</h2>
-    <ul>
-        <li>
-            Mango
-        </li>
-        <li>
-            Fresa
-        </li>
-        <li>
-            Chocolate
-        </li>
-    </ul>
-    <h2>Adornos</h2>
-    <ul>
-        <li>
-          Betún
-        </li>
-        <li>
-            Glaseado
-        </li>
-    </ul>
-</div>
-</div>
+        <label class="margin-right10" for="sabor">Nombre</label>
+        <input type="text" />
+        <br />
+        <br />
+        <label class="margin-right10" for="sabor">Correo Electrónico</label>
+        <input type="text" />
+        <h2>Informacion del pastel</h2>
 
+        <p>Escoge uno</p>
+        <input type="text" />
+        <br />
+        <label class="margin-right10" for="sabor">Sabor de pastel</label>
+
+        <p>Escoge uno</p>
+        <input type="text" />
+        <br />
+        <label for="sabor">Adorno del pastel</label>
+        <div>
+          <button class="margin-top10">Enviar pedido</button>
+        </div>
+      </form>
+    </div>
+    <div class="mitad">
+      <h2 class="title">Sabores</h2>
+      <ul>
+        <li>Mango</li>
+        <li>Fresa</li>
+        <li>Chocolate</li>
+      </ul>
+      <h2>Adornos</h2>
+      <ul>
+        <li>Betún</li>
+        <li>Glaseado</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 
@@ -52,23 +54,23 @@ export default {};
 </script>
 
 <style>
-.mitad{
-    width: 50%;
+.mitad {
+  width: 50%;
 }
 
-.layout{
-display: flex;
-
+.layout {
+  display: flex;
 }
 
-.margin-top10{
-    margin-top:10px;
+.margin-top10 {
+  margin-top: 10px;
+}
+.margin-right10 {
+  margin-right: 10px;
 }
 
-ul li{
-    list-style: none;
-    text-align: initial;
-
+ul li {
+  list-style: none;
+  text-align: initial;
 }
-
 </style>
