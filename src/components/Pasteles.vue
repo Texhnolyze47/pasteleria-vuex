4bf98e1c8be61bac71176412c0c3d4f88506601c
Texhnolyze <ichigobankai136@gmail.com>
2022-03-24 19:16:55 -0600

    style: se dio formato al componente pasteles

--- src/components/Pasteles.vue
+++ src/components/Pasteles.vue
@@ -1,105 +1,124 @@
 <template>
-   <div class="title">
-             <h2>Pasteles</h2>
-        </div>   
-    <div class="container">
-     
-        <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/pastel-mango-durazno.jpeg" alt="">
-            <h2 class="title">Pasteles de mango</h2>
-                        <p><strong>300$</strong></p>
-
-        </div>
-         <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/will-echols-P_l1bJQpQF0-unsplash.jpg" alt="">
-            <h2 class="title">Pasteles de chocolate</h2>
-                        <p><strong>300$</strong></p>
-
-
-        </div>
-         <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/coffeefy-workafe-tk0S-jtcjv4-unsplash.jpg" alt="">
-            <h2 class="title">Pasteles de nuez</h2>
-                        <p><strong>300$</strong></p>
-
-
-        </div>
-              <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/pastel-mango-durazno.jpeg" alt="">
-            <h2 class="title">Pasteles de mango</h2>
-                        <p><strong>300$</strong></p>
-
-
-        </div>
-         <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/will-echols-P_l1bJQpQF0-unsplash.jpg" alt="">
-            <h2 class="title">Pasteles de chocolate</h2>
-                        <p><strong>300$</strong></p>
-
-
-        </div>
-         <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/coffeefy-workafe-tk0S-jtcjv4-unsplash.jpg" alt="">
-            <h2 class="title">Pasteles de nuez</h2>
-                        <p><strong>300$</strong></p>
-
-        </div>
-         <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/pastel-mango-durazno.jpeg" alt="">
-            <h2 class="title">Pasteles de mango</h2>
-                        <p><strong>300$</strong></p>
-
-        </div>
-         <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/will-echols-P_l1bJQpQF0-unsplash.jpg" alt="">
-            <h2 class="title">Pasteles de chocolate</h2>
-                        <p><strong>300$</strong></p>
-
-
-        </div>
-         <div class="frame">
-            <img  class="imagen-container" src="../assets/pasteles/coffeefy-workafe-tk0S-jtcjv4-unsplash.jpg" alt="">
-            <h2 class="title">Pasteles de nuez</h2>
-            <p><strong>300$</strong></p>
-
-        </div>
+  <div class="title">
+    <h2>Pasteles</h2>
+  </div>
+  <div class="container">
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/pastel-mango-durazno.jpeg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de mango</h2>
+      <p><strong>300$</strong></p>
     </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/will-echols-P_l1bJQpQF0-unsplash.jpg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de chocolate</h2>
+      <p><strong>300$</strong></p>
+    </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/coffeefy-workafe-tk0S-jtcjv4-unsplash.jpg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de nuez</h2>
+      <p><strong>300$</strong></p>
+    </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/pastel-mango-durazno.jpeg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de mango</h2>
+      <p><strong>300$</strong></p>
+    </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/will-echols-P_l1bJQpQF0-unsplash.jpg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de chocolate</h2>
+      <p><strong>300$</strong></p>
+    </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/coffeefy-workafe-tk0S-jtcjv4-unsplash.jpg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de nuez</h2>
+      <p><strong>300$</strong></p>
+    </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/pastel-mango-durazno.jpeg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de mango</h2>
+      <p><strong>300$</strong></p>
+    </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/will-echols-P_l1bJQpQF0-unsplash.jpg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de chocolate</h2>
+      <p><strong>300$</strong></p>
+    </div>
+    <div class="frame">
+      <img
+        class="imagen-container"
+        src="../assets/pasteles/coffeefy-workafe-tk0S-jtcjv4-unsplash.jpg"
+        alt=""
+      />
+      <h2 class="title">Pasteles de nuez</h2>
+      <p><strong>300$</strong></p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-.title{
-    margin-top: 15px;
-    color: #ff759f;
+.title {
+  margin-top: 15px;
+  color: #ff759f;
 }
-.container{
-    margin-top: 20px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    width: 100%;
-    height: auto;
+.container {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  height: auto;
 }
-.frame{
-    margin-top: 10px;
-    margin-left: 150px;
-    width: 300px;
-    height: 480px;
-}
-
-.imagen-container{
-    margin-top: 10px;
-    width: 200px;
-    height: 200px;
+.frame {
+  margin-top: 10px;
+  margin-left: 150px;
+  width: 300px;
+  height: 480px;
 }
 
-.text-justify{
-    Text-justify: auto;
-    padding: 10px;
+.imagen-container {
+  margin-top: 10px;
+  width: 200px;
+  height: 200px;
+}
+
+.text-justify {
+  text-justify: auto;
+  padding: 10px;
 }
 </style>
