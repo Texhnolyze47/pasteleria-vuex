ff7bf0fc1453b0330f087c3549e42d5592213b8a
Texhnolyze <ichigobankai136@gmail.com>
2022-03-25 16:06:17 -0600

    add: se agrego formulario del pastel

---
+++ src/components/Form.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="layout">
+    <div class="mitad">
+    <form id="form-pe">
+      <p>Escoge uno </p>
+  <input type="text" value="Sabor" />
+  <br/>
+  <label for="sabor">Sabor de pastel</label>
+
+        <p>Escoge uno </p>
+  <input type="text" value="Adorno" />
+  <br/>
+  <label for="sabor">Adorno del pastel</label>
+
+   
+  </form>
+  
+</div>
+<div class="mitad">
+    <h2 class="title">Sabores</h2>
+    <ul>
+        <li>
+            Mango
+        </li>
+        <li>
+            Fresa
+        </li>
+        <li>
+            Chocolate
+        </li>
+    </ul>
+    <h2>Adornos</h2>
+    <ul>
+        <li>
+          Betún
+        </li>
+        <li>
+            Glaseado
+        </li>
+    </ul>
+</div>
+</div>
+
+</template>
+
+
+
+<script>
+export default {};
+</script>
+
+<style>
+.mitad{
+    width: 50%;
+}
+
+.layout{
+display: flex;
+
+}
+
+ul li{
+    list-style: none;
+    text-align: initial;
+
+}
+
+</style>
--- src/views/CustomPastel.vue
+++ src/views/CustomPastel.vue
@@ -1,10 +1,18 @@
 <template>
 <h2>Personaliza tu pastel</h2>  
+<Form></Form>
+<Footer></Footer>
+
 </template>
 
 <script>
+import Form from '../components/Form.vue'
+import Footer from '../components/Footer.vue'
 export default {
-
+components:{
+    Form,
+    Footer
+}
 }
 </script>
 
