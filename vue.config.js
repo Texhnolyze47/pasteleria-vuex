0ad8e1bc75ce6ab13ed5ea58a56fd95e044f9fbe
Texhnolyze <ichigobankai136@gmail.com>
2022-03-31 12:42:19 -0600

    Revert "fix:se modifico el vue config para el deploy"
    This reverts commit baa2b8092c866d23328587206c05dd8091b36d79.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath : '/vuex-pasteleria/'
+  transpileDependencies: true
 })
